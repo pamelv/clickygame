62cf077f3d414f2ca8292a7c83fb26a54e26db1f
Pamela Vasquez <pamelavasquez94@gmail.com>
2020-04-11 14:12:17 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static("client/build"));
 app.get("/ping", function (req, res) {
   return res.send("pong");
 });
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve("client", "build", "index.html"));
 });
-app.listen(port);
+
+app.listen(port, () => {
+  console.log("Sufs up");
+});
